42d1e18afe2aa84d08c3d9244ae427718458de54
hetianyi <hehety@outlook.com>
2019-03-21 17:56:54 +0800

    add test

---
+++ src/lib/util_test.go
@@ -0,0 +1,26 @@
+package lib
+
+import (
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"testing"
+)
+
+func Test1(t *testing.T) {
+	f, _ := GetFile("C:\\Users\\hehety\\Downloads\\folder_document_233px_1185632_easyicon.net.png")
+	bs, _ := ioutil.ReadAll(f)
+	fmt.Print("[]byte{")
+	for i, v := range bs {
+		if i == len(bs)-1 {
+			fmt.Print(v)
+		} else {
+			fmt.Print(v, ",")
+		}
+	}
+	fmt.Println("}")
+}
+func Test2(t *testing.T) {
+	a := []string{"1.jpg", "11.jpg", "2.jpg"}
+	fmt.Println(a)
+}
